a560060ab2ce46c016af8e31452ae2d3b5aeff8b
AJ Guiles <ashiguiles@gmail.com>
2019-03-26 03:03:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
-class Dog 
- 
- Def bark
-  puts "Woof!"
-end
 
- Def sit
-  puts "The Dog is sitting"
-end
+class Dog
+  def bark
+    puts "Woof!"
+  end
 
+  def sit
+    puts "The Dog is sitting"
+  end
 end
